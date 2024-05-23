676e368a14a26532edf83f4121bc412412a94089
Kasa-27119 <kasa27119@gmail.com>
2024-05-23 18:37:55 +1200

    fixed JS functions, on-clicks, population, added mapbox and media queries for tablet and mobile, and finished CSS styling

--- script.js
+++ script.js
@@ -1,63 +1,20 @@
+// mapbox token
+mapboxgl.accessToken = "pk.eyJ1IjoiY2lhcmFuc2xvdyIsImEiOiJjbHY0ZW91YnYwOGV3MmlwOGQ5b3l3a3J3In0.EFWZEAWA13ehFAw5jdLqJA";
+
 // initialise jQuery script
-$(document).ready(function() {
+$(document).ready(function () {
 
     // jquery functions here
     // $("p").css("color", "white"); // jquery test
 });
 
 // clothing array 
-const clothingItems = [
-    {
-        id: 1,
-        name: "Baseball Jacket",
-        price: "$30.99",
-        category: "jackets",
-        color: "Blue",
-        sizes: ["XS", "S", "M", "L"],
-        image: ["imgs/bear-baseball-jacket1.jpg", "imgs/bear-baseball-jacket2.jpg"],
-    },
-    {
-        id: 2,
-        name: "Black Cat Bag",
-        price: "$23.99",
-        category: "accessories",
-        color: "Mixed",
-        sizes: "ONE SIZE",
-        image: ["https://i.pinimg.com/originals/99/7c/b6/997cb6ce95bf4f05f78c2d3d313ea375.jpg", "https://i.pinimg.com/originals/5b/58/fa/5b58fa650c08314c363f79da3368f8e7.jpg"],
-    },
-    {
-        id: 3,
-        name: "Blue Knitted Sweater",
-        price: "$21.99",
-        category: "tops",
-        color: "Blue",
-        sizes: ["M", "L", "XL"],
-        image: ["imgs/blue-sweater-1.jpg", "imgs/blue-sweater-2.jpg"],
-    },
-    {
-        id: 4,
-        name: "Cat T-Shirt",
-        price: "$18.99",
-        category: "tops",
-        color: "Black",
-        sizes: ["XS", "S", "M", "L", "XL"],
-        image: ["imgs/cat-black-tee1.jpg", "imgs/cat-black-tee2.jpg"],
-    },
-    {
-        id: 5,
-        name: "Cherry T-Shirt",
-        price: "$18.99",
-        category: "tops",
-        color: "Mixed",
-        sizes: ["XS", "S", "M", "L", "XL"],
-        image: ["imgs/cherry-tee1.jpg", "imgs/cherry-tee2.jpg"],
-    },
-    {
+const clothingItems = [{
         id: 6,
         name: "Frog Hat",
         price: "$12.99",
-        category: "accessories",
-        color: "Green",
+        category: "Accessories",
+        color: ["Green", "Pink"],
         sizes: "ONE SIZE",
         image: ["https://i.etsystatic.com/23931016/r/il/9ca001/2918979529/il_1140xN.2918979529_fhlu.jpg", "https://i.etsystatic.com/28947382/r/il/dddb8b/3037038704/il_1588xN.3037038704_dccf.jpg"],
     },
@@ -65,19 +22,64 @@ const clothingItems = [
         id: 7,
         name: "Pink Jeans",
         price: "$24.99",
-        category: "bottoms",
+        category: "Bottoms",
         color: "Pink",
         sizes: ["S", "M", "L"],
         image: ["https://www.wholesalejeans.to/wp-content/uploads/2021/11/16364203131.png", "https://i.pinimg.com/originals/ca/bb/86/cabb869a9de508beae891e17c60fc44f.png"],
     },
     {
+        id: 3,
+        name: "Blue Knitted Sweater",
+        price: "$21.99",
+        category: "Tops",
+        color: "Blue",
+        sizes: ["M", "L", "XL"],
+        image: ["imgs/blue-sweater-1.jpg", "imgs/blue-sweater-2.jpg"],
+    },
+    {
+        id: 5,
+        name: "Cherry T-Shirt",
+        price: "$18.99",
+        category: "Tops",
+        color: "Mixed",
+        sizes: ["XS", "S", "M", "L", "XL"],
+        image: ["imgs/cherry-tee1.jpg", "imgs/cherry-tee2.jpg"],
+    },
+    {
         id: 8,
         name: "Pattern Denim Jacket",
         price: "$24.99",
-        category: "jacket",
+        category: "Jacket",
         color: "Blue",
         sizes: ["M", "L"],
         image: ["imgs/pattern-denim-jacket1.jpg", "imgs/pattern-denim-jacket2.jpg"],
+    },
+    {
+        id: 2,
+        name: "Black Cat Bag",
+        price: "$23.99",
+        category: "Accessories",
+        color: "Mixed",
+        sizes: "ONE SIZE",
+        image: ["https://i.pinimg.com/originals/99/7c/b6/997cb6ce95bf4f05f78c2d3d313ea375.jpg", "https://i.pinimg.com/originals/5b/58/fa/5b58fa650c08314c363f79da3368f8e7.jpg"],
+    },
+    {
+        id: 1,
+        name: "Baseball Jacket",
+        price: "$30.99",
+        category: "Jackets",
+        color: ["Blue", "Mixed"],
+        sizes: ["XS", "S", "M", "L"],
+        image: ["imgs/bear-baseball-jacket1.jpg", "imgs/bear-baseball-jacket2.jpg"],
+    },
+    {
+        id: 4,
+        name: "Cat T-Shirt",
+        price: "$18.99",
+        category: "Tops",
+        color: "Black",
+        sizes: ["XS", "S", "M", "L", "XL"],
+        image: ["imgs/cat-black-tee1.jpg", "imgs/cat-black-tee2.jpg"],
     }
 ]
 
@@ -93,121 +95,173 @@ const lowToHighSortButton = document.getElementById("price-low-to-high-sort");
 const highToLowSortButton = document.getElementById("price-high-to-low-sort");
 
 // on click (change) function - filter selectors
+// filter on click - clothing type
 clothingTypeFilter.addEventListener("change", function() {
+    filterAndPopulateCardContainer();
     console.log("type filter click is working");
-    console.dir(clothingTypeFilter.value);
-    // filter and then populate the product container div - function goes here
 })
 
+// filter on click - clothing size
 clothingSizeFilter.addEventListener("change", function() {
+    filterAndPopulateCardContainer();
     console.log("size filter click is working");
-    console.dir(clothingSizeFilter.value);
-    // filter and then populate the product container div - function goes here
 })
 
+// filter on click - clothing color
 clothingColorFilter.addEventListener("change", function() {
+    filterAndPopulateCardContainer();
     console.log("color filter click is working");
-    console.dir(clothingColorFilter.value);
-    // filter and then populate the product container div - function goes here
 })
 
-// filter functions
+
+
+// on click function - sorting buttons
+// sort on click - alphabet sort button
+alphabeticalSortButton.addEventListener("click", function () {
+
+    // getting the filtered array first
+    const filteredClothingItems = filterClothingItems();
+
+    // running the sorting function on the filtered array
+    const sortedClothingItems = sortClothingItemsAlphabetical(filteredClothingItems);
+
+    // then populate the cards with the filtered and sorted array
+    populateCardContainer(sortedClothingItems);
+
+    console.log("alphabet sort btn working");
+})
+
+// sort on click - price low to high button
+lowToHighSortButton.addEventListener("click", function () {
+
+    // getting the filtered array first
+    const filteredClothingItems = filterClothingItems();
+
+    // running the sorting function on the filtered array
+    const sortedClothingItems = sortClothingItemsLowToHigh(filteredClothingItems);
+
+    // then populate the cards with the filtered and sorted array
+    populateCardContainer(sortedClothingItems);
+
+    console.log("low to high sort btn working");
+})
+
+// sort on click - price high to low button
+highToLowSortButton.addEventListener("click", function () {
+
+    // getting the filtered array first
+    const filteredClothingItems = filterClothingItems();
+
+    // running the sorting function on the filtered array
+    const sortedClothingItems = sortClothingItemsHighToLow(filteredClothingItems);
+
+    // then populate the cards with the filtered and sorted array
+    populateCardContainer(sortedClothingItems);
+
+    console.log("high to low sort btn working");
+})
+
+
 function filterClothingItems() {
-
-    // return filtered clothing items as an array = filteredClothingItems
     const filteredClothingItems = clothingItems.filter(clothingItem => {
-
-        // main filter - clothing type
-        // set both filter value and clothing type to be exactly the same - set to both lowercase first
-
         // clothing item value and property
         const clothingItemType = clothingItem.category.toLowerCase();
         const clothingTypeFilterValue = clothingTypeFilter.value.toLowerCase();
 
         // clothing item size value and property
-        const clothingItemSize = clothingItem.sizes.toLowerCase();
-        const clothingSizeFilterValue = clothingTypeFilter.value.toLowerCase();
+        const clothingItemSize = clothingItem.sizes;
+        const clothingSizeFilterValue = clothingSizeFilter.value.toUpperCase();
 
         // clothing item color value and property
-        const clothingItemColor = clothingItem.color.toLowerCase();
+        const clothingItemColor = clothingItem.color;
         const clothingColorFilterValue = clothingColorFilter.value.toLowerCase();
 
-
-        // if statements - if clothing detail and filter values DON't MATCH
+        // if statements - if clothing detail and filter values DON'T MATCH
 
         // if the clothing item type DOESN'T include the filter value - do not add to the array
         if (clothingTypeFilterValue && !clothingItemType.includes(clothingTypeFilterValue)) {
-
             return false;
         }
 
         // if the item size doesn't include the filter value - do not add to the array
-        if (clothingSizeFilterValue && !clothingItemSize.includes(clothingSizeFilterValue)) {
-            return false;
+        if (clothingSizeFilterValue) {
+            if (Array.isArray(clothingItemSize)) {
+                if (!clothingItemSize.includes(clothingSizeFilterValue)) {
+                    return false;
+                }
+            } else {
+                if (clothingItemSize !== clothingSizeFilterValue) {
+                    return false;
+                }
+            }
         }
 
         // if the item color doesn't match the filter value  - do not add to the array
-        if (clothingColorFilterValue && !clothingItemColor.includes(clothingColorFilterValue)) {
-            return false;
+        if (clothingColorFilterValue) {
+            if (Array.isArray(clothingItemColor)) {
+                if (!clothingItemColor.map(c => c.toLowerCase()).includes(clothingColorFilterValue)) {
+                    return false;
+                }
+            } else {
+                if (clothingItemColor.toLowerCase() !== clothingColorFilterValue) {
+                    return false;
+                }
+            }
         }
 
         // if all of item properties match the filter value
         return true;
-   
-    })
-    // and add the to filteredProperties array
-    return filteredClothingItems;
+    });
 
-    
+    // return the filtered array
+    return filteredClothingItems;
 }
+
 
 // sorting functions
 // sorting alphabetically
-function sortClothingItemsAlphabetical (clothingItems) {
+function sortClothingItemsAlphabetical(clothingItems) {
     return clothingItems.sort((a, b) => {
         const itemNameA = a.name.toLowerCase();
         const itemNameB = b.name.toLowerCase();
 
-        return itemNameA.localCompare(itemNameB);
+        return itemNameA.localeCompare(itemNameB);
     })
 }
 
 // sort price low to high
-function sortClothingItemsLowToHigh (clothingItems) {
+function sortClothingItemsLowToHigh(clothingItems) {
     return clothingItems.sort((a, b) => {
 
         // turning the item price into an integer and removing the dollar sign and decimal
-        const itemPriceA = parseFloat(a.price.replace(/\$/g, "").replace(/./g, ""));
-        const itemPriceB = parseFloat(b.price.replace(/\$/g, "").replace(/./g, ""));
+        const itemPriceA = parseFloat(a.price.replace(/\$/g, ""));
+        const itemPriceB = parseFloat(b.price.replace(/\$/g, ""));
 
         return itemPriceA - itemPriceB;
     })
 }
 
 // sort price high to low
-function sortClothingItemsHighToLow (clothingItems) {
+function sortClothingItemsHighToLow(clothingItems) {
     return clothingItems.sort((a, b) => {
 
         // turning the item price into an integer and removing the dollar sign and decimal
-        const itemPriceA = parseFloat(a.price.replace(/\$/g, "").replace(/./g, ""));
-        const itemPriceB = parseFloat(b.price.replace(/\$/g, "").replace(/./g, ""));
+        const itemPriceA = parseFloat(a.price.replace(/\$/g, ""));
+        const itemPriceB = parseFloat(b.price.replace(/\$/g, ""));
 
         return itemPriceB - itemPriceA;
     })
 }
 
-
-
-
-
-
-
-
-
+// FILTER AND POPULATE PRODUCT CARD CONTAINER
+function filterAndPopulateCardContainer() {
+    const filteredClothingItems = filterClothingItems();
+    populateCardContainer(filteredClothingItems);
+}
 
 
 // initial population of cards
-function populateCardContainer (clothingItems) {
+function populateCardContainer(clothingItems) {
 
     // get product card container first
     const productCardContainer = document.getElementById("product-card-container");
@@ -284,3 +338,24 @@ const swiper = new Swiper('.swiper', {
         clickable: true, // enables pagination bullets
     },
 });
+
+// mapbox initialisation
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/navigation-night-v1', // style URL
+    center: [-122.170031285158, 37.70496989833294], // starting position [lng, lat]
+    zoom: 15,
+});
+
+// getting the map marker
+const customMarker = document.getElementById('custom-marker-1');
+
+// setting up the mapbox marker
+new mapboxgl.Marker(customMarker)
+    .setLngLat([-122.170031285158, 37.70496989833294]) // Specify the coordinates of the marker
+
+    // setting a pop-up on the marker
+    .setPopup(new mapboxgl.Popup({offset: 25}).setHTML("<h1>AdoreEco Clothing Store</h1><p>1922 Republic Ave, San Leandro, CA 94577, USA</p>"))
+    .addTo(map);
+
+    
